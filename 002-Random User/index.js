0778f7cdc524ab22b5bf778975d0e2aa6e100eb8
lucamarchisiooo <71224681+lucamarchisiooo@users.noreply.github.com>
2021-01-17 19:26:20 +0100

    icon menu

--- 002-Random User/index.js
+++ 002-Random User/index.js
@@ -1,4 +1,5 @@
 "use strict"
+let naz = ["AU" , "BR" , "CA" , "CH" , "DE" , "DK" , "ES" , "FI" , "FR" , "GB" , "IE" , "IR" , "NO" , "NL" , "NZ" , "TR" , "US"];
 window.onload = function () {
     let _icon = document.getElementById("icon");
     var slider = document.getElementById("num");
@@ -8,22 +9,27 @@ window.onload = function () {
         output.innerText = this.value;
     }
     let _divChk = document.getElementById("divChk");
-    let naz = ["AU" , "BR" , "CA" , "CH" , "DE" , "DK" , "ES" , "FI" , "FR" , "GB" , "IE" , "IR" , "NO" , "NL" , "NZ" , "TR" , "US"];
     for (let i = 0; i<naz.length ; i++){
-        _divChk.innerHTML += naz[i] +"<input type=\"checkbox\" checked=\"checked\">"+"<span class=\"checkmark\"></span>\n"
+        _divChk.innerHTML += naz[i] +"<input class=\"nation\" id=" + naz[i] + " type=\"checkbox\" checked=\"checked\">"+"<span class=\"checkmark\"></span>\n"
     }
 }
 
 function genUsers(){
     let num = document.getElementById("num").value;
     let param = "?results=" + num;
-    let url = "https://randomuser.me/api/" + param;
-    let _name = document.getElementById("user_value").value;
-    let _user
+    let gender = document.getElementById("gender").value;
+    param += "&gender=" + gender;
+    var nation =  [document.getElementsByClassName("nation").length];
+    for (let i = 0; i<nation.length; i++){
+
+    }
+    param += "&nat=" ;
+    var url = "https://randomuser.me/api/" + param;
     $.ajax({
         url: url,
         success: function(data) {
-            console.log(data);
+            nation = data[1];
+            console.log(_name);
         }
     });
 }
